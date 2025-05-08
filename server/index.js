d01ffd4fd376e47766ce1415e21574db16b8fbdb
Ivan <ivanlazarov2003@outlook.com>
2025-05-08 20:37:58 +0200

    Uptaded server logic to work with supabase

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ const db = new pg.Client({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // This allows connection to Supabase
+  },
 });
 db.connect();
 
